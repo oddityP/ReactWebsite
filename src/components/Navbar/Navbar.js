0e2ccf701748e32a2b10d60964e531ddd5da092d
oddityP <82304903+oddityP@users.noreply.github.com>
2022-03-31 00:57:27 +0530

    navbar done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+import { Button } from '../../globalStyles';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks, NavItemBtn, NavBtnLink } from './NavbarElements';
 
 const Navbar = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
 
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
   return (
       <>
       <IconContext.Provider value={{ color: '#fff'}}>
@@ -20,13 +35,36 @@ const Navbar = () => {
               {/* if i click the MobileIcon, I want the FaTimes to be displayed, and the FaBars to be displayed otherwise ie when not true */}
               {click ? <FaTimes /> : <FaBars />} 
             </MobileIcon>
-            <NavMenu onClick={handleClick}>
+            <NavMenu onClick={handleClick} click={click}>
               <NavItem>
                 <NavLinks to='/'>
                   Home
                 </NavLinks>
               </NavItem>
+              <NavItem>
+                <NavLinks to='/services'>
+                  Services
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='/products'>
+                  Products
+                </NavLinks>
+              </NavItem>
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to='/sign-up'>
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to='/sign-up'>
+                    <Button fontBig primary>
+                      SIGN UP
+                    </Button>
 
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
             </NavMenu>
           </NavbarContainer>
         </Nav>
