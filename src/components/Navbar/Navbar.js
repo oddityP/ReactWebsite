586c71187c65220d71fbf2dc63273a2891533e3e
oddityP <82304903+oddityP@users.noreply.github.com>
2022-03-30 17:27:17 +0530

    NavIcon added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { Nav, NavbarContainer, NavLogo, NavIcon } from './NavbarElements';
+import React, {useState} from 'react';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './NavbarElements';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
   return (
       <>
+      <IconContext.Provider value={{ color: '#fff'}}>
         <Nav>
           <NavbarContainer>
             <NavLogo to='/'>
               <NavIcon />
               ULTRA
             </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {/* if i click the MobileIcon, I want the FaTimes to be displayed, and the FaBars to be displayed otherwise ie when not true */}
+              {click ? <FaTimes /> : <FaBars />} 
+            </MobileIcon>
           </NavbarContainer>
         </Nav>
+      </IconContext.Provider>
       
       </>
   );
