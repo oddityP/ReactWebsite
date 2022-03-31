e6c637fcb4f25ca69e3cbf3f5ccdb62227257b1b
oddityP <82304903+oddityP@users.noreply.github.com>
2022-03-31 17:41:02 +0530

    created home oject one

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Button } from '../../globalStyles';
+import { InfoSec, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, Subtitle } from './InfoSectionElements';
+
+const InfoSection = ( { primary, lightBg, imgStart, lightTopLine, lightTextDesc, buttonLabel, description, headline, lightText, topLine }) => {
+  return (
+      <>
+        <InfoSec lightBg={lightBg}>
+            <Container>
+                <InfoRow imgStart={imgStart}>
+                    <InfoColumn>
+                        <TextWrapper>
+                            <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                            <Heading lightText={lightText}>{headline}</Heading>
+                            <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                            <Link to='/sign-up'>
+                                <Button big fontBig primary={primary}>{buttonLabel}</Button>
+                            </Link>
+                        </TextWrapper>
+                    </InfoColumn>
+
+                </InfoRow>
+            </Container>
+        </InfoSec>
+      </>
+  );
+};
+
+export default InfoSection;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,13 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true, 
+    lightBg: false, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'We help you generate new braincells by giving you a platform to learn any language', 
+    headline: 'Online Language Learning Platform for Global Citizens', 
+    lightText: true, 
+    topLine: 'The next Duolingo Owl'
+};
